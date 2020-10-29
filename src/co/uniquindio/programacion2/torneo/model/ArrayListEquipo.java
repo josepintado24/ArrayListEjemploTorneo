fdc903cf4c19d2ad3aa1e040894f70231681625a
Jose Pintado <i201716389@cibertec.edu.pe>
2020-10-28 20:55:06 -0500

    Ejercicios hasta la 4

--- src/co/uniquindio/programacion2/torneo/model/ArrayListEquipo.java
+++ src/co/uniquindio/programacion2/torneo/model/ArrayListEquipo.java
@@ -15,8 +15,14 @@ public class ArrayListEquipo {
 		//Primer equipo
 		listaEquipos.add(equipo01);
 		EstadisticaEquipo estadistica01=new EstadisticaEquipo();
-		estadistica01.setPartidosGanados(5);
 		
+		estadistica01.setPartidosJugados(6);
+		estadistica01.setPartidosGanados(5);
+		estadistica01.setPartidosEmpatados(3);
+		estadistica01.setPartidosPerdidos(4);
+		estadistica01.setGolesAFavor(5);
+		estadistica01.setGolesEnContra(8);
+		estadistica01.setPuntosTotales(50);
 		directortec=new DirectorTecnico();
 		directortec.setNombre("Juan");
 		directortec.setAniosExperienci(5);
@@ -27,7 +33,14 @@ public class ArrayListEquipo {
 		//2do equipo
 		listaEquipos.add(equipo02);
 		EstadisticaEquipo estadistica02=new EstadisticaEquipo();
-		estadistica02.setPartidosGanados(1);
+		estadistica02.setPartidosJugados(1);
+		estadistica02.setPartidosGanados(5);
+		estadistica02.setPartidosEmpatados(3);
+		estadistica02.setPartidosPerdidos(4);
+		estadistica02.setGolesAFavor(5);
+		estadistica02.setGolesEnContra(8);
+		estadistica02.setPuntosTotales(60);
+		
 		DirectorTecnico directortec02=new DirectorTecnico();
 		directortec02.setNombre("Manuel");
 		directortec02.setAniosExperienci(1);
@@ -37,7 +50,14 @@ public class ArrayListEquipo {
 		//3er equipo
 		listaEquipos.add(equipo03);
 		EstadisticaEquipo estadistica03=new EstadisticaEquipo();
+		
+		estadistica03.setPartidosJugados(10);
 		estadistica03.setPartidosGanados(5);
+		estadistica03.setPartidosEmpatados(3);
+		estadistica03.setPartidosPerdidos(4);
+		estadistica03.setGolesAFavor(5);
+		estadistica03.setGolesEnContra(8);
+		estadistica03.setPuntosTotales(9);
 		DirectorTecnico directortec03=new DirectorTecnico();
 		directortec03.setNombre("Efra");
 		directortec03.setAniosExperienci(4);
@@ -78,26 +98,57 @@ public class ArrayListEquipo {
 		return listaDirector;
 	}
 	
-	public String se() {
-		ArrayList<Equipo> listaDirector=new ArrayList<Equipo>();
-		String salida ="";
-		for (int i=0;i<tamanio() ;i++) {
-			if (listaEquipos.get(i).getDirectortec().getAniosExperienci()>3) {
-				String nombreDirec=listaEquipos.get(i).getDirectortec().getNombre();
-				String primeravocal=nombreDirec.charAt(0)+"";
-				String ultimogmeravocal=nombreDirec.charAt(nombreDirec.length()-1)+"";
-				if ("A".equals(primeravocal) ||"E".equals(primeravocal) || "I".equals(primeravocal) || "O".equals(primeravocal) || "U".equals(primeravocal)){
-					if ("a".equals(ultimogmeravocal)|| "e".equals(ultimogmeravocal)|| "i".equals(ultimogmeravocal) || "o".equals(ultimogmeravocal) ||"u".equals(ultimogmeravocal)){
-						//listaDirector.add(listaEquipos.get(i));
-						salida=primeravocal;
-					}
-				}
-				
-				
+	/*
+	 * String [] [] animals = {
+                {"DanaDog", "WallyDog", "JessieDog", "AlexisDog", "LuckyDog"},
+                {"BibsCat", "DoodleCat", "MillieCat", "SimonCat"},
+                {"ElyFish", "CloieFish", "GoldieFish", "OscarFish", "ZippyFish", "TedFish"}
+                };
+	 * */
+	public String[][] estadisticaTorneo(){
+
+		String [] [] estadisticaTotal =new String[listaEquipos.size()][8];
+		
+		for (int i=0;i<estadisticaTotal.length;i++) {
+			estadisticaTotal[i][0]=obtener(i).getNombre();
+			estadisticaTotal[i][1]=obtener(i).getEstadistica().getPartidosJugados()+"";
+			estadisticaTotal[i][2]=obtener(i).getEstadistica().getPartidosGanados()+"";
+			estadisticaTotal[i][3]=obtener(i).getEstadistica().getPartidosEmpatados()+"";
+			estadisticaTotal[i][4]=obtener(i).getEstadistica().getPartidosPerdidos()+"";
+			estadisticaTotal[i][5]=obtener(i).getEstadistica().getGolesAFavor()+"";
+			estadisticaTotal[i][6]=obtener(i).getEstadistica().getGolesEnContra()+"";
+			estadisticaTotal[i][7]=obtener(i).getEstadistica().getPuntosTotales()+"";
+		}
+		return estadisticaTotal;
+	}
+	
+	public String[][] estadisticasEquipoMayorPuntaje(){
+		String [] [] estadisticaTotal =new String[2][2];
+		int puntajeMayor=Integer.parseInt(estadisticaTorneo()[0][7]);
+		String nombreEquipo=estadisticaTorneo()[0][0];
+		for (int i=0;i<tamanio();i++) {
+			int Puntaje =Integer.parseInt(estadisticaTorneo()[i][7]);
+			if (Puntaje>puntajeMayor) {
+				puntajeMayor=Puntaje;
+				nombreEquipo=estadisticaTorneo()[i][0];
 			}
 		}
-		return salida;
-		
+		estadisticaTotal[0][0]=nombreEquipo;
+		estadisticaTotal[0][1]=puntajeMayor+"";
+		return estadisticaTotal;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- src/co/uniquindio/programacion2/torneo/test/Testjugador.java
+++ src/co/uniquindio/programacion2/torneo/test/Testjugador.java
@@ -9,14 +9,36 @@ public class Testjugador {
 		//Posicion tipo=Posicion.VOLANTE;	
 		//Jugador jugador01=new Jugador("001", "Luis", 20,tipo);
 		//Cliente cliente00001=new Cliente("1223456","Jose", "M","Lima",tipo);
-		
 		ArrayListEquipo prueba =new ArrayListEquipo();
+		/*
+		 * 2.	Realizar las operaciones necesarias para retornar un listado de equipos (ArrayList) con los equipos que tengan un director técnico que cuente con más de 3 años de experiencia y su nombre (el del técnico) empiece y termine en vocal, asimismo que haya ganado (el equipo) más de tres partidos en el torneo.
+		 
+		
 		
 		for (int a=0;a<prueba.aniosExDirector().size();a++) {
 			System.out.println(prueba.aniosExDirector().get(a).toString());
-		}
+		}*/
+		
+		/*3.	Realizar las operaciones necesarias para retornar una matriz con las estadísticas de cada equipo del torneo, cada fila representa un equipo y las columnas representa los datos de las estadísticas del equipo.
+		System.out.print("Equipo\t\tPJ\tPG\tPE\tPP\tGF\tGE\tPT\n");
+		for (int i=0;i<prueba.estadisticaTorneo().length;i++) {
+			System.out.print("\n");
+			for (int j=0;j<8;j++) {
+				
+				
+				System.out.print(prueba.estadisticaTorneo()[i][j]+"\t");
+			}
+		}*/
 		
 		
+		/*
+		 4.	Realizar las operaciones necesarias para consultar el nombre del equipo que tiene mayor puntaje en el torneo, se debe usar la funcionalidad del punto 3, buscando en la matriz de estadísticas el equipo con mayor puntaje.
+		 */
+				System.out.print("Equipo\t\tPT\n");
+				System.out.print(
+						prueba.estadisticasEquipoMayorPuntaje()[0][0]+"\t"+
+						prueba.estadisticasEquipoMayorPuntaje()[0][1]
+								);
 	}
 
 }
